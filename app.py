2ae90b1b1c9dbf8a7b9d40c252ea645708beab45
Mohamed Mahmoud Ali <45339003+Thesnak@users.noreply.github.com>
2023-01-01 03:23:54 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, render_template, url_for, jsonify
+from tensorflow.keras.models import load_model
+from PIL import Image
+import numpy as np
+
+app = Flask(__name__)
+
+def preprossing(image):
+    image=Image.open(image)
+    image = image.resize((150, 150))
+    image_arr = np.array(image.convert('RGB'))
+    image_arr.shape = (1, 150, 150, 3)
+    return image_arr
+
+classes = ['Buildings' ,'Forest', 'Glacier' ,'Mountain' ,'Sea' ,'Street']
+model=load_model("Intel_Image_Classification.h5")
+
+@app.route('/')
+def index():
+
+    return render_template('index.html', appName="Intel Image Classification")
+
+
+@app.route('/predictApi', methods=["POST"])
+def api():
+    # Get the image from post request
+    try:
+        if 'fileup' not in request.files:
+            return "Please try again. The Image doesn't exist"
+        image = request.files.get('fileup')
+        image=image.read()
+        image_arr, dimage = preprossing(image)
+        print("Model predicting ...")
+        result = model.predict(image_arr)
+        print("Model predicted")
+        ind = np.argmax(result)
+        prediction = classes[ind]
+        print(dimage)
+        print(prediction)
+        return jsonify({'prediction': prediction})
+    except:
+        return jsonify({'Error': 'Error occur'})
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    print("run code")
+    if request.method == 'POST':
+        # Get the image from post request
+        print("image loading....")
+        image = request.files['fileup']
+        print("image loaded....")
+        image_arr= preprossing(image)
+        print("predicting ...")
+        result = model.predict(image_arr)
+        print("predicted ...")
+        ind = np.argmax(result)
+        prediction = classes[ind]
+
+        print(prediction)
+
+        return render_template('index.html', prediction=prediction, image='static/IMG/', appName="Intel Image Classification")
+    else:
+        return render_template('index.html',appName="Intel Image Classification")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
